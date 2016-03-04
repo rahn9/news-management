a097b024e8dc5505cb18ecbae61134b9f0c9d445
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-04 16:12:01 +0600

    [imp] test added

--- src/test/java/com/epam/vakhidat/news_management/action/AddNewsActionTest.java
+++ src/test/java/com/epam/vakhidat/news_management/action/AddNewsActionTest.java
@@ -1,5 +1,6 @@
 package com.epam.vakhidat.news_management.action;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -9,5 +10,11 @@ public class AddNewsActionTest {
     @Test
     public void testExecute() throws Exception {
         assertTrue(true);
+    }
+
+    @Ignore
+    @Test
+    public void testIgnored() throws Exception {
+        assertTrue(false);
     }
 }
